16c10907291ca9228ce1c218470dba815561f16d
sarkaromar <ausantemonir@gmial.com>
2019-01-13 01:06:42 +0600

    Pages create done

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -24,4 +24,77 @@ class FrontController extends Controller
     public function whyus(){
         return view('front.pages.whyus');
     }
+
+    public function services(){
+        return view('front.pages.services');
+    }
+
+    public function it_consulting(){
+        return view('front.pages.it_consulting');
+    }
+    
+    public function biz_automation(){
+        return view('front.pages.biz_automation');
+    }
+    
+    public function software_dev(){
+        return view('front.pages.software_dev');
+    }
+    
+    public function data_analysis(){
+        return view('front.pages.data_analysis');
+    }
+    
+    public function web_portal(){
+        return view('front.pages.web_portal');
+    }
+
+    public function ecommerce(){
+        return view('front.pages.ecommerce');
+    }
+    
+    public function outcourced(){
+        return view('front.pages.outcourced');
+    }
+    
+    public function mobile_app(){
+        return view('front.pages.mobile_app');
+    }
+    
+    public function web_apps_dev(){
+        return view('front.pages.web_apps_dev');
+    }
+    
+    public function qa(){
+        return view('front.pages.qa');
+    }
+
+    public function image_analysis(){
+        return view('front.pages.image_analysis');
+    }
+    
+    public function networking(){
+        return view('front.pages.networking');
+    }
+    
+    public function security(){
+        return view('front.pages.security');
+    }
+    
+    public function products(){
+        return view('front.pages.products');
+    }
+    public function portfolio(){
+        return view('front.pages.portfolio');
+    }
+    public function news(){
+        return view('front.pages.news');
+    }
+    public function contact(){
+        return view('front.pages.contact');
+    }
+    public function free_consultant(){
+        return view('front.pages.free_consultant');
+    }
+
 }
---
+++ resources/views/front/pages/qa.blade.php
@@ -0,0 +1,181 @@
+@extends('front.layouts.master')
+
+@section('content')
+
+	<!-- Content -->
+	<div class="section section-contents section-pad">
+        <div class="container">
+            <div class="content row">
+
+                <div class="col-sm-7 npl res-m-bttm">
+                    <h2>We have experience to deliver the best services you need to move forward.</h2>
+                    <p>Through gaining experience in various economic periods, our team are able to identify the best opportunities whether we are in good times or bad times. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tiudo lorem quveniamv eniam laud accusan tiud.</p>
+                    <p>We achieved our success because of how successfully we integrate with our clients. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam.</p>
+                </div>
+                <div class="col-sm-5 npr">
+                    <img alt="" src="image/service-f.jpg">
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- End content -->
+    <!-- Content -->
+    <div class="section section-contents section-pad bg-light">
+        <div class="container">
+            <div class="content row center">
+                <h2 class="heading-section">Our Smart Approach</h2>
+                <div class="gaps size-sm"></div>
+                <div class="row">
+                    <div class="col-sm-4 res-s-bttm">
+                        <div class="box bg-white pd-x3 round">
+                            <div class="icon-box style-s1">
+                                <em class="fa fa-diamond" aria-hidden="true"></em>
+                            </div>
+                            <h4>Experienced Consultant</h4>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tiudo lorem quveniamv eniam laud accusan tiud.</p>
+                        </div>
+                        
+                    </div>
+                    <div class="col-sm-4 res-s-bttm">
+                        <div class="box bg-white pd-x3 round">
+                            <div class="icon-box style-s1">
+                                <em class="fa fa-check-square" aria-hidden="true"></em>
+                            </div>
+                            <h4>Reliable &amp; Trustworthy</h4>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tiudo lorem quveniamv eniam laud accusan tiud.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="box bg-white pd-x3 round">
+                            <div class="icon-box style-s1">
+                                <em class="fa fa-bolt" aria-hidden="true"></em>
+                            </div>
+                            <h4>20 years of experience</h4>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tiudo lorem quveniamv eniam laud accusan tiud.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End content -->
+    <!-- Why Choose -->
+    <div class="section section-contents section-pad light has-bg dark-filter fixed-bg" style="background-image: url(image/plx-dark.jpg);">
+        <div class="container">
+            <div class="content row center">
+
+                <h2 class="heading-section">Why Choose Us</h2>
+                <div class="gaps size-sm"></div>
+                <div class="feature-intro">
+
+                    <div class="row">
+                        <div class="col-sm-3 res-s-bttm">
+                            <div class="icon-box style-s2">
+                                <em class="fa fa-briefcase" aria-hidden="true"></em>
+                            </div>
+                            <h4>Business Service Consulting</h4>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tiudo lorem quveniamv eniam.</p>
+                        </div>
+                        <div class="col-sm-3 res-s-bttm">
+                            <div class="icon-box style-s2">
+                                <em class="fa fa-black-tie" aria-hidden="true"></em>
+                            </div>
+                            <h4>Financial Services Consulting</h4>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tiudo lorem quveniamv eniam.</p>
+                        </div>
+                        <div class="col-sm-3 res-s-bttm">
+                            <div class="icon-box style-s2">
+                                <em class="fa fa-cogs" aria-hidden="true"></em>
+                            </div>
+                            <h4>Advanced Analytics &amp; Reports</h4>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tiudo lorem quveniamv eniam.</p>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="icon-box style-s2">
+                                <em class="fa fa-building" aria-hidden="true"></em>
+                            </div>
+                            <h4>Quality &amp; Innovation Solutions</h4>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tiudo lorem quveniamv eniam.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>			
+        </div>
+    </div>
+    <!-- End Why Choose -->
+
+    <!-- Testimonials -->
+    <div class="section section-quotes section-pad bg-light">
+        <div class="container">
+            <div class="content row center">
+            
+                <h2 class="heading-section">What Our Client Say's</h2>
+                <div class="gaps size-sm"></div>
+                <div class="testimonials">
+                    <div id="testimonial" class="quotes-slider quotes-boxed col-md-8 col-md-offset-2">
+                        <div class="owl-carousel loop has-carousel" data-items="1" data-loop="true" data-dots="true" data-auto="true">
+                            <div class="item">
+                                <!-- Each Quotes -->
+                                <div class="quotes">
+                                    <div class="quotes-text">
+                                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam.</p>
+                                    </div>
+                                    <div class="profile-thumb">
+                                        <img src="image/client-a.jpg" alt="">
+                                    </div>
+                                    <div class="profile">
+                                        <h5>John Doe</h5>
+                                        <h6>CEO, Company Name</h6>
+                                    </div>
+                                </div>
+                                <!-- End Quotes -->
+                            </div>
+                            <!-- // -->
+                            <div class="item">
+                                <!-- Each Quotes -->
+                                <div class="quotes">
+                                    <div class="quotes-text">
+                                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo Nemo enim ipsam.</p>
+                                    </div>
+                                    <div class="profile-thumb">
+                                        <img src="image/client-b.jpg" alt="">
+                                    </div>
+                                    <div class="profile">
+                                        <h5>John Doe</h5>
+                                        <h6>CEO, Company Name</h6>
+                                    </div>
+                                </div>
+                                <!-- End Quotes -->
+                            </div>
+                        </div>
+                        <!-- End LoopSlide -->
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <!-- End Section -->
+
+    <!-- Client logo -->
+    <div class="section section-logos section-pad-sm bdr-top">
+        <div class="container">
+            <div class="content row">
+
+                <div class="owl-carousel loop logo-carousel">
+                    <div class="logo-item"><img alt="" width="190" height="82" src="image/cl-logo1.png"></div>
+                    <div class="logo-item"><img alt="" width="190" height="82" src="image/cl-logo2.png"></div>
+                    <div class="logo-item"><img alt="" width="190" height="82" src="image/cl-logo3.png"></div>
+                    <div class="logo-item"><img alt="" width="190" height="82" src="image/cl-logo4.png"></div>
+                    <div class="logo-item"><img alt="" width="190" height="82" src="image/cl-logo5.png"></div>
+                    <div class="logo-item"><img alt="" width="190" height="82" src="image/cl-logo6.png"></div>
+                </div>
+
+            </div>
+        </div>	
+    </div>
+    <!-- End Section -->
+@endsection
